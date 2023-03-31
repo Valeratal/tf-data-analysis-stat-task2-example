5160c0e1a66698668475d503fb9ece1381dd2e5b
Valeratal <test4you@yandex.ru>
2023-03-31 11:43:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,21 @@ from scipy.stats import norm
 
 chat_id = 252926140 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), loc - scale * norm.ppf(alpha / 2)
+def proportion_confidence_interval(p: float, n: int, alpha: float = 0.05) -> tuple:
+    """
+    Calculate the confidence interval for a population proportion.
+
+    Parameters:
+    p (float): the proportion of successes in the sample
+    n (int): the sample size
+    alpha (float): the level of significance (default is 0.05)
+
+    Returns:
+    tuple: a tuple containing the lower and upper bounds of the confidence interval
+    """
+    z = norm.ppf(1 - alpha / 2)
+    phat = p
+    se = np.sqrt(p * (1 - p) / n)
+    lower = phat - z * se
+    upper = phat + z * se
+    return (lower, upper)
