11ab2a696a3817f0d240a1ed52a5dcd11fed3020
Valeratal <test4you@yandex.ru>
2023-04-05 14:14:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,29 +1,22 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import norm
+from scipy.stats import uniform
 
 
 chat_id = 252926140 # Ваш chat ID, не меняйте название переменной
 
 
-def solution(p: float, x: np.ndarray) -> tuple:
-    """
-    Calculates the confidence interval for the significance level
-    given a confidence level and an array of significance levels.
-
-    Parameters:
-        p (float): Confidence level, a number between 0 and 1.
-        x (numpy.ndarray): One-dimensional array of significance levels.
-
-    Returns:
-        Tuple of two values representing the left and right boundaries of the
-        confidence interval for the significance level.
-    """
+def solution(p: float, x: np.array) -> tuple:
+    n = len(x)
     alpha = 1 - p
-    lower_bound = np.quantile(x, alpha/2)
-    upper_bound = np.quantile(x, 1 - alpha/2)
-    return (max(lower_bound, 0.047), min(upper_bound, 1.0))
-
+    z_alpha = uniform.ppf(alpha, loc=0.047, scale=b-0.047)
+    b = np.max(x)
+    se = np.sqrt(b*(1-b)/n)
+    z_se = se * uniform.ppf(1-alpha/2, loc=0, scale=1)
+    left = max(0.047, b - z_se)
+    right = min(1, b - z_alpha)
+    return (left, right)
 
 
 
